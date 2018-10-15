7b5c60545c7d8e97e37bb9d0be4c1593657c31eb
Jia Li <Jia.Li@astrum-it.de>
2018-10-15 09:37:07 +0200

    Initial commit

---
+++ src/TestTemplate.java
@@ -0,0 +1,13 @@
+public class TestTemplate {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        String s1 = new String();
+        for (int i = 0; i < 3; i++) {
+            System.out.println();
+        }
+
+    }
+}
